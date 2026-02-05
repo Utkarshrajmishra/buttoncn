effbdb33a69bd651f04fc5ec873e487d69b1a6f1
Utkarshrajmishra <utkarshrajmishra811545@gmail.com>
2026-02-05 11:59:27 +0530

    Fix: Next Config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,29 @@
 import { createMDX } from 'fumadocs-mdx/next';
 
+
 /** @type {import('next').NextConfig} */
 const config = {
-  reactStrictMode: false,
+  reactStrictMode: true,
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  outputFileTracingIncludes:{
+    "/**":["components/buttoncn/**/**"]
+  },
+  async headers(){
+    return[
+      {
+        source:"/r/:path",
+        headers:[
+          {
+            key:"Cache-Control",
+            value:"public, max-age=31536000, immutable"
+          }
+        ]
+      }
+    ]
+  },
+  eslint:{
+    ignoreDuringBuilds:true,
+  }
 };
 
 const withMDX = createMDX({});
